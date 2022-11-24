0475fe939219954e7c6b95a485dcd7f666c7912f
Karan <karansethia24@gmail.com>
2022-11-25 00:15:27 +0530

    Learning Middleware

--- 5-WorkingwithExpress/app.js
+++ 5-WorkingwithExpress/app.js
@@ -5,6 +5,14 @@ const http = require('http');
 const app = express();
 console.log("working with express");
 
+//? use allows use to add a new middleware function
+app.use((req,res,next) => {  //? next is a function passed to the 'use' method to be executed to allow reques travel to another method 
+    console.log("inside middleware");
+    next();  //? this allows the request to proceed to next middleware otherwise it will skip that middleware
+});
+app.use((req,res,next) => { 
+    console.log("inside second middleware");
+});
 const server = http.createServer(app);
 
 server.listen(3000);
