a00f3872d5aedfb047f6a273611f35d15418d79f
Karan <karansethia24@gmail.com>
2022-11-30 22:36:45 +0530

    Serving HTML Pages

--- 8-ExpressRouter/routes/admin.js
+++ 8-ExpressRouter/routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 //? We use express.Router() used to create a new router object. This function is used when you want to create a new router object in your program to handle requests.
 const router = express.Router();
 
@@ -15,14 +16,17 @@ const router = express.Router();
 
 
 
-router.get('/add-product',(req,res,next) => {
-        console.log("inside /add-product path");
-        res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Send</button</form>')
-    });
-router.post('/product',(req,res,next) => {
-    console.log("inside /products path");
-    console.log(req.body); 
+router.get('/add-product', (req, res, next) => {
+    // res.send(
+    //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+    // );
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+  });
+  
+  // /admin/add-product => POST
+  router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
     res.redirect('/');
-});
+  });
 
 module.exports = router;
--- 8-ExpressRouter/routes/shop.js
+++ 8-ExpressRouter/routes/shop.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
 
 //? If we use router.get instead of router.use then the browser will redirect here only on exact match of '/' path
 router.get('/',(req,res,next) => {
     console.log("inside / path");
-    res.send('<h1>in the default response</h1>');
+    // res.send('<h1>in the default response</h1>');
+    //? Using sendfile to send html
+    // ! res.sendFile('./views/shop.html') => this will not work since / in here points towards the root directory of the computer
+    res.sendFile(path.join(__dirname,'../','views','shop.html'))
 });
 
 module.exports = router;
